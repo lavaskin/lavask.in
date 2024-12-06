e9bcf3f0272f5b1eb1048c785abeba1d682fae23
Jackson E <lavaskin@gmail.com>
2024-12-05 16:36:32 -0800

    add basic links

--- src/app/pages/cs/449/449.component.ts
+++ src/app/pages/cs/449/449.component.ts
@@ -8,8 +8,14 @@ import { Link } from 'src/app/models/link.model';
 })
 export class Four49Component implements OnInit {
 	public title: string = 'lavask.in';
-	public links: Link[] = [];
 	public hoveredIndex: number | null = null;
+
+	public links: Link[] = [
+		{ title: 'csfloat listing', href: 'https://csfloat.com/item/784934779127729881', color: '#343744', },
+		{ title: 'steam', href: 'https://steamcommunity.com/profiles/76561198121030123', color: '#171a21', },
+		{ title: 'trade', href: 'https://steamcommunity.com/tradeoffer/new/?partner=160764395&token=vwcpkFVk', color: '#1d5676' },
+		{ title: 'cashrep', href: 'https://csgo-rep.com/profile/76561198121030123', color: '#00d632' },
+	];
 	
 	ngOnInit(): void { }
 
